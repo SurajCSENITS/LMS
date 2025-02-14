1c108258ce9b4388dc07d000e480c7a91d5b72dd
SurajCSENITS <138098058+SurajCSENITS@users.noreply.github.com>
2025-02-14 20:12:00 +0530

    Update axiosInstance.js

--- client/src/Helpers/axiosInstance.js
+++ client/src/Helpers/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL= "https://lms-server-inky.vercel.app/api/v1";
+const BASE_URL= "https://lms-server-ghvg.onrender.com/api/v1";
 
 const axiosInstance= axios.create();
 axiosInstance.defaults.baseURL= BASE_URL;
